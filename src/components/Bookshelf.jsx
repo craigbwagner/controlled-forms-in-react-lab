2647ae3fa5d20f6c20606767ceca02c2cafb84c4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-24 15:40:39 -0400

    add controlled inputs

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -5,16 +5,44 @@ function Bookshelf() {
 		{ title: 'Fourth Wing', author: 'Rebecca Yarros' },
 		{ title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
   ]);
+  const [newBook, setNewBook] = useState({
+		title: '',
+		author: '',
+  });
 
-	return (
+  function handleInputChange(e) {
+		setNewBook({ ...newBook, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {}
+
+  return (
 		<div className='bookshelfDiv'>
 			<div className='formDiv'>
 				<h3>Add a Book</h3>
-				{/* Form will go here */}
+				<form onSubmit={handleSubmit}>
+					<label htmlFor='title'>Title: </label>
+					<input
+						type='text'
+						name='title'
+						id='title'
+						value={newBook.title}
+						onChange={handleInputChange}
+					></input>
+					<label htmlFor='author'>Author: </label>
+					<input
+						type='text'
+						name='author'
+						id='author'
+						value={newBook.author}
+						onChange={handleInputChange}
+					></input>
+					<button type='submit'>Add Book</button>
+				</form>
 			</div>
 			<div className='bookCardsDiv'>{/* Book cards will display here */}</div>
 		</div>
-	);
+  );
 }
 
 export default Bookshelf;
